4fc7391fc3d3a6e1351788cd69ac368e7df58206
Jay Massena <jay@3meters.com>
2016-08-20 07:37:43 -0700

    Finish support to limit push notifications to new messages only

--- Patchrapp/src/main/java/com/patchr/components/NotificationService.java
+++ Patchrapp/src/main/java/com/patchr/components/NotificationService.java
@@ -16,8 +16,23 @@ public class NotificationService extends NotificationExtenderService {
 	@Override
 	protected boolean onNotificationProcessing(OSNotificationReceivedResult notification) {
 
+		JSONObject data = notification.payload.additionalData;
+		String pushNotificationType = Patchr.settings.getString(StringManager.getString(R.string.pref_push_notification_type)
+			, StringManager.getString(R.string.pref_push_notification_type_default));
+
+		if ("none".equals(pushNotificationType)) {
+			return true;
+		}
+		else if ("messages_only".equals(pushNotificationType)) {
+			if (data != null) {
+				String eventType = data.optString("event");
+				if (!"insert_entity_message_content".equals(eventType)) {
+					return true;
+				}
+			}
+		}
+
 		if (notification.isAppInFocus) {
-			JSONObject data = notification.payload.additionalData;
 			if (data != null) {
 				String targetId = data.optString("targetId");
 				String parentId = data.optString("parentId");
